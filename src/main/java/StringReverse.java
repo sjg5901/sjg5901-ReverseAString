243099c93a1bb7c3b4bae27c105625d90ec808f9
Skyler Giannetti <sjg5901@psu.edu>
2023-08-14 02:19:55 +0000

    Reverse a String challenge complete

--- src/main/java/StringReverse.java
+++ src/main/java/StringReverse.java
@@ -1,4 +1,6 @@
 
+
+
 public class StringReverse {
     /**
      * Task: return the reverse of a string.
@@ -13,6 +15,6 @@ public class StringReverse {
      * @return the reverse of str.
      */
     public String reverse(String str){
-        return null;
+        return new StringBuilder(str).reverse().toString();
     }
 }
